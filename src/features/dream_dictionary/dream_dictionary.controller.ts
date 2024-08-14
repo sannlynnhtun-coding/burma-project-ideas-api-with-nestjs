1dd7ceab32509a35f89dbbd92bc91c2fe755d9fb
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-08-14 13:20:12 +0630

    fix: update dream dictionary

--- src/features/dream_dictionary/dream_dictionary.controller.ts
+++ src/features/dream_dictionary/dream_dictionary.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { DreamDictionaryService } from './dream_dictionary.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('dream-dictionary')
 @Controller('dream-dictionary')
 export class DreamDictionaryController {
   constructor(private readonly dreamDictionaryService: DreamDictionaryService) {}
