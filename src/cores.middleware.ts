ba98c2e0792518a9cf285d8c9f76d1c9edab4d63
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-12-15 19:38:26 +0630

    fix: code changes

--- src/cores.middleware.ts
+++ src/cores.middleware.ts
@@ -5,7 +5,7 @@ import { Request, Response, NextFunction } from 'express';
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
-    res.setHeader('Access-Control-Allow-Methods', 'GET'); // Allow common methods
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Allow common methods
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Allow common headers
 
     if (req.method === 'OPTIONS') {
